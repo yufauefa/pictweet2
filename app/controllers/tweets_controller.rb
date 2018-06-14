52270614d75a91784c09b94c47818e5a6850a1f6
Miho Kodama <k.mihob1028@gmail.com>
2018-06-14 20:17:00 +0900

    recreate: css, application.html.erb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,18 @@
 class TweetsController < ApplicationController
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.order('created_at DESC')
   end
+
+  def new
+  end
+
+def create
+  Tweet.create(tweet_params)
+end
+
+private
+def tweet_params
+  params.permit(:name, :image, :text)
+end
+
 end
